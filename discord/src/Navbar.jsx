75ab2a23f0ce9207bb5c38b6c57cebf986867fe8
Michael Nguyen <michael.nguyen@live.nl>
2024-04-27 17:16:21 +0200

    navbar donev1

--- discord/src/Navbar.jsx
+++ discord/src/Navbar.jsx
@@ -4,7 +4,7 @@ import menu from "./assets/menu-icon.png";
 
 function Navbar() {
   return (
-    <header>
+ 
       <nav>
         <div>
           <img src={discordlogo} alt="Logo" className="logo" />
@@ -13,7 +13,6 @@ function Navbar() {
           <img src={menu} alt="menu" className="menu-icon" />
         </div>
       </nav>
-    </header>
   );
 }
 
